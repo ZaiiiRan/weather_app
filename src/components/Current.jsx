dfe1ab26b94971d8e9cd8f621c4de86a3937da8c
ZaiiiRan <a1b075753@gmail.com>
2024-04-12 11:28:32 +0300

    fix convert degs

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,14 +1,15 @@
 import './Current.css'
 
 function convertDegsToCompass(degs) {
-    if (degs === 0) return 'В'
-    else if (degs > 0 && degs < 90) return 'С-В'
-    else if (degs === 90) return 'С'
-    else if (degs > 90 && degs < 180) return 'С-З'
-    else if (degs === 180) return 'З'
-    else if (degs > 180 && degs < 270) return 'Ю-З'
-    else if (degs === 270) return 'Ю'
-    else return 'Ю-В'
+    if (degs >= 0 && degs < 22) return 'С'
+    else if (degs >= 22 && degs <= 67) return 'С-В'
+    else if (degs > 67 && degs < 112) return 'В'
+    else if (degs >= 112 && degs <= 157) return 'Ю-В'
+    else if (degs > 157 && degs < 202) return 'Ю'
+    else if (degs >= 202 && degs <= 247) return 'Ю-З'
+    else if (degs > 247 && degs < 292) return 'З'
+    else if (degs >= 292 && degs <= 337) return 'С-З'
+    else return 'С'  
 }
 
 export default function Current({city, weatherData}) {
