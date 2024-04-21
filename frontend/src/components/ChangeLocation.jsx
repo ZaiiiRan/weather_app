a1c6788db4e05d6fb8ac4e8696ee586758182a9f
ZaiiiRan <a1b075753@gmail.com>
2024-04-21 20:55:59 +0300

    change location

--- frontend/src/components/ChangeLocation.jsx
+++ frontend/src/components/ChangeLocation.jsx
@@ -5,13 +5,11 @@ export default function ChangeLocation({setCity, isDay = true}) {
     const [isHovered, setIsHovered] = useState(false)
     const [isFocused, setIsFocused] = useState(false)
     const [isExpanded, setIsExpanded] = useState(false)
+    const [text, setText] = useState('')
     return (
-        <div className='ChangeLocationButton' style={{
+        <div className={`ChangeLocationButton ${isExpanded ? 'show' : ''}`} style={{
             backgroundColor: (isDay) ? 'rgba(0, 0, 0, 0.178)' : 'rgba(255, 255, 255, 0.178)',
             transform: isHovered && !isExpanded ? 'scale(110%)' : 'none',
-            width: isExpanded ? '380px' : '80px',
-            height: '80px',
-            borderRadius: isExpanded ? '25px' : '50px',
         }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={(event) => {
                     if (event.target.closest('.ChangeLocationInput')) return
                     setIsExpanded(!isExpanded)
@@ -24,12 +22,13 @@ export default function ChangeLocation({setCity, isDay = true}) {
                     backgroundColor: (isDay) ? 'rgba(0, 0, 0, 0.178)' : 'rgba(255, 255, 255, 0.178)'
                 }} disabled={!isHovered} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} onKeyDown={(event) => {
                     if (isFocused && event.key === 'Enter') {
-                        setCity(event.target.value)
+                        setCity(text)
+                        setText('')
                     }
-                }}/>
+                }} onChange={(event) => setText(event.target.value)}/>
                 <div className='ChangeLocationInput-submit' style={{
                     backgroundColor: (isDay) ? 'rgba(0, 0, 0, 0.178)' : 'rgba(255, 255, 255, 0.178)',
-                }}>ОК</div>
+                }} onClick={ () => {setCity(text); setText('')}}>ОК</div>
             </div>
         </div>
     )
